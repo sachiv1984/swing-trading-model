3bd2624e0c60af3c086dffc9a9f59cb147f4cc0f
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-01-24 15:36:00 +0000

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "../utils/utils";
 import { motion, AnimatePresence } from "framer-motion";
 import { 
   LayoutDashboard, 
