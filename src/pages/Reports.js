8dd605da4b3e2fc78579f04145f8f22a94014c4f
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-01-24 17:04:28 +0000

    Update Reports.js

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,6 +1,6 @@
 import { useState, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { base44 } from "@/api/base44Client";
+import { base44 } from "../api/base44Client";
 import { 
   FileText, 
   Download, 
