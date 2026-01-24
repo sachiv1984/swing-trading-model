d6fd33ba4e8c7b6611943e26c0276fc0c9f75f1e
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-01-24 17:34:10 +0000

    Update PositionModal.js

--- src/components/positions/PositionModal.js
+++ src/components/positions/PositionModal.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 import { format, differenceInDays } from "date-fns";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 export default function PositionModal({ position, open, onClose, onSave }) {
   const [formData, setFormData] = useState({
