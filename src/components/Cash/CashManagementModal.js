534e73953fa368fc6f72f7c7b187d262092fe488
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-01-31 15:58:24 +0000

    Update CashManagementModal.js

--- src/components/Cash/CashManagementModal.js
+++ src/components/Cash/CashManagementModal.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "../../api/base44Client";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { PlusCircle, MinusCircle, Loader2 } from "lucide-react";
 import { cn } from "../../lib/utils";
 
