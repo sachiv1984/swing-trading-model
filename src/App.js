70546085e918bd4c44c357d82f454397cfde1f64
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-02-08 12:32:13 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Toaster } from "../components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
+import { queryClientInstance } from '../lib/query-client'
 import NavigationTracker from '../lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
