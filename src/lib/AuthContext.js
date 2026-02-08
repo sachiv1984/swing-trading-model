e7a9acf9e1fa36683db27f9d17a3a1970d09073f
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-02-08 15:13:05 +0000

    Update AuthContext.js

--- src/lib/AuthContext.js
+++ src/lib/AuthContext.js
@@ -43,4 +43,19 @@ export const AuthProvider = ({ children }) => {
       isLoadingPublicSettings,
       authError,
       appPublicSettings,
-      logo
+      logout,
+      navigateToLogin,
+      checkAppState
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
