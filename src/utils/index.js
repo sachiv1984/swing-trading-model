06ccf45ed46d1ae4481db9eb2cf69de629d8f2d1
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-02-08 10:06:55 +0000

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,3 @@
-export const createPageUrl = (pageName) => {
-  const routes = {
-    Dashboard: '/',
-    Positions: '/positions',
-    TradeEntry: '/trade-entry',
-    TradeHistory: '/trade-history',
-    Reports: '/reports',
-    Settings: '/settings',
-  };
-  return routes[pageName] || '/';
-};
+export function createPageUrl(pageName: string) {
+    return '/' + pageName.replace(/ /g, '-');
+}
