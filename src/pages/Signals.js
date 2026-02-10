2acc530a62f04cd1eb604c8763912662dd1dead8
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-02-10 10:03:30 +0000

    Update Signals.js

--- src/pages/Signals.js
+++ src/pages/Signals.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "..tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "../api/base44Client";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
