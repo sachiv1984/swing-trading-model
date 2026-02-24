9928c9df485c2a5907b8987735fa31031cfdcbf0
sachiv1984 <sachiv.patel@hotmail.co.uk>
2026-02-24 09:40:00 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ from config import ALLOWED_ORIGINS
 from utils.calculations import calculate_initial_stop
 from utils.formatting import decimal_to_float
 from pydantic import BaseModel
-from routers import validation, analytics, test, portfolio_size
+from routers import validation, analytics, test, portfolio_size, trades_export
+
 
 
 from database import (
@@ -110,6 +111,7 @@ app.include_router(validation.router)
 app.include_router(analytics.router)
 app.include_router(test.router)
 app.include_router(portfolio_size.router)
+app.include_router(trades_export.router)
 
 
 @app.get("/")
